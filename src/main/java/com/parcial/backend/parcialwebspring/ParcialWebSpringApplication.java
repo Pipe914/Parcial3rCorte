72435a96d35e4f3807e1b09b9ad08ab0c87bfd41
ANDRES FELIPE AFANADOR SALAZAR <andresafsa@unisabana.edu.co>
2021-05-29 17:08:44 -0500

    Base spring app

---
+++ src/main/java/com/parcial/backend/parcialwebspring/ParcialWebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.parcial.backend.parcialwebspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialWebSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parcial/backend/parcialwebspring/ParcialWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial.backend.parcialwebspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
