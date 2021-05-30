b124a959871fac8629c32c1d782a2cac744582ed
David <davidbahe01@gmail.com>
2021-05-30 18:27:03 -0500

    No me funciona pero bueno

--- src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
+++ src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
@@ -10,12 +10,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
     @GetMapping("/")
     public String home(){
-        return "login/Index";
+        return "Index";
     }
-  
+    @GetMapping("/aspirante")
+    public String ingresoAs(){
+        return "aspirante";   
+    }
+    @GetMapping("/administradoe")
+    public String ingresoAd(){
+        return "administrador";   
+    }
+    @GetMapping("/buscarOferta")
+    public String buscarOferta(){
+        return "buscarOferta"; 
+    }
+    @GetMapping("/crearEmpresa")
+    public String crearEmpresa(){
+        return "crearEmpresa"; 
+    }
+    @GetMapping("/crearOferta")
+    public String crearOferta(){
+        return "crearOferta"; 
+    }
+    @GetMapping("/modificarOferta")
+    public String modificarOferta(){
+        return "modificarOferta"; 
+    }
+    
+
+
+    
     @PostMapping("/prueba")
     public String verifyLogin(@RequestParam(name = "user", required = false, defaultValue = "no envio nada") String user, @RequestParam(name = "pass", required = false, defaultValue = "no envio nada") String password){
         return "";
     }
+    
     
 }
