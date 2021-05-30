206b3fc9f976af2b3d5d0f5407c4ad29db942a05
ANDRES FELIPE AFANADOR SALAZAR <andresafsa@unisabana.edu.co>
2021-05-30 18:26:26 -0500

    Cambios pre pull david: vistas completas

--- src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
+++ src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
@@ -1,21 +1,37 @@
 package com.parcial.backend.parcialwebspring.controlador;
 
+import com.parcial.backend.parcialwebspring.services.proxy.Login;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
-    @GetMapping("/")
-    public String home(){
-        return "login/Index";
+    @GetMapping(value = "/")
+    public String home() {
+        return "Index";
     }
-  
-    @PostMapping("/prueba")
-    public String verifyLogin(@RequestParam(name = "user", required = false, defaultValue = "no envio nada") String user, @RequestParam(name = "pass", required = false, defaultValue = "no envio nada") String password){
-        return "";
+
+    @PostMapping(value = "/login")
+    @ResponseBody
+    public String login(@RequestParam(name = "userLogin", required = true, defaultValue = "no envio nada") String user,
+            @RequestParam(name = "passLogin", required = true, defaultValue = "no envio nada") String password,
+            @RequestParam(name = "tipologin", required = true, defaultValue = "no envio nada") String tipo) {
+        System.out.println(user + "  " + password + " " + tipo);
+        Login controladorLogin = Login.getLogin();
+        return "se logueo";
     }
-    
+
+    @PostMapping(value = "/registro")
+    @ResponseBody
+    public String asdf(
+            @RequestParam(name = "userRegistro", required = false, defaultValue = "no envio nada") String user,
+            @RequestParam(name = "passRegistro", required = false, defaultValue = "no envio nada") String password) {
+        System.out.println(user + "  " + password);
+        return "se registro";
+    }
+
 }
