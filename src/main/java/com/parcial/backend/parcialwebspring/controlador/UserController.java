3f6400cf24983d1f0fcc74bd2b71a76144a7fec3
ANDRES FELIPE AFANADOR SALAZAR <andresafsa@unisabana.edu.co>
2021-05-30 15:17:34 -0500

    Carpetas completas

--- src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
+++ src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
@@ -2,9 +2,11 @@ package com.parcial.backend.parcialwebspring.controlador;
 
 import com.parcial.backend.parcialwebspring.services.Prueba;
 
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+
 public class UserController {
     @GetMapping("/")
     public String home(){
@@ -22,17 +25,20 @@ public class UserController {
         return "Login";
     }
     
-    @PostMapping("/")
-    //@ResponseBody
+    @PostMapping("/prueba")
     public String verifyLogin(@RequestParam(name = "user", required = false, defaultValue = "no envio nada") String user, @RequestParam(name = "pass", required = false, defaultValue = "no envio nada") String password){
-        
-        /*Prueba xd = new Prueba();
+        Prueba xd = new Prueba();
         if(xd.verifyL(user, password)){
             return "Login";
         }else{
             return "<h1>Intente Nuevamente</h1>";
-        }*/
-        
-        return "Home";
+        }
+
     }
+    @RequestMapping("Login")
+      public String postBody(@RequestBody String fullName) {
+        return "Hello " + fullName;
+    }   
+    
+
 }
