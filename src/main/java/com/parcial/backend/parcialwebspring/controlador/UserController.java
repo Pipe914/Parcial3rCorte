ce61fa6f302d9f68670de439a993d68f56a10245
ANDRES FELIPE AFANADOR SALAZAR <andresafsa@unisabana.edu.co>
2021-05-30 22:59:58 -0500

    Pre pull david: metodos y carptas

--- src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
+++ src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
@@ -53,7 +53,7 @@ public class UserController {
     }
 
     @GetMapping("modificarOferta.html")
-    public String modificarOferta() {
+    public String modificarOfertaVista() {
         return "administrador/modificarOferta";
     }
 
@@ -73,40 +73,118 @@ public class UserController {
     }
 
     @PostMapping(value = "/registro")
-    public String asdf(
-            @RequestParam(name = "userRegistro", required = true, defaultValue = "") String username,
+    public String asdf(@RequestParam(name = "userRegistro", required = true, defaultValue = "") String username,
             @RequestParam(name = "passRegistro", required = true, defaultValue = "") String password,
             RedirectAttributes redirectAttrs) {
         System.out.println(username + "  " + password);
 
         user = controlador.createUser("1", username, password);
         if (user != null) {
-            redirectAttrs.addFlashAttribute("mensaje", "Se ha registrado correctamente!!").addFlashAttribute("clase", "success");
+            redirectAttrs.addFlashAttribute("mensaje", "Se ha registrado correctamente!!").addFlashAttribute("clase",
+                    "success");
             return "redirect:/";
         } else {
-            redirectAttrs.addFlashAttribute("mensaje", "Registro fallido, intente nuevamente").addFlashAttribute("clase", "success");
+            redirectAttrs.addFlashAttribute("mensaje", "Registro fallido, intente nuevamente")
+                    .addFlashAttribute("clase", "success");
             return "redirect:/";
         }
 
     }
 
     @PostMapping(value = "/crearEmpresa")
-    @ResponseBody
     public String crearEmpresa(
             @RequestParam(name = "nombre", required = true, defaultValue = "no envio nada") String nombre,
             @RequestParam(name = "nit", required = true, defaultValue = "no envio nada") String nit,
-            @RequestParam(name = "direccion", required = true, defaultValue = "No envio") String direccion) {
+            @RequestParam(name = "direccion", required = true, defaultValue = "No envio") String direccion,
+            RedirectAttributes redirectAttrs) {
 
         System.out.println(nombre + "  " + nit + " " + direccion);
         String response = AESEncript.decrypt(
                 controlador.action(AESEncript.encrypt("3/" + key + "/" + nombre + "/" + nit + "/" + direccion)));
         System.out.println(response);
         if (response.equals("La empresa se ha creado exitosamente")) {
-            return "se creo correctamente";
+            redirectAttrs.addFlashAttribute("mensaje", "Empresa creada correctamente").addFlashAttribute("clase",
+                    "success");
+            return "redirect:/crearEmpresa.html";
         } else {
-            return "No se creo correctamente";
+            redirectAttrs.addFlashAttribute("mensaje", "Operacion fallida. Razón: " + response)
+                    .addFlashAttribute("clase", "success");
+            return "redirect:/crearEmpresa.html";
         }
+    }
 
+    @PostMapping(value = "/crearOfertaBase")
+    public String crearOfertaBase(RedirectAttributes redirectAttrs) {
+
+        String response = AESEncript.decrypt(controlador.action(AESEncript.encrypt("4/" + key)));
+        System.out.println(response);
+
+        if (response.equals("Usted no tiene acceso para realizar esta acción!!!")
+                || response.equals("La sesion no existe, ha caducado.")) {
+            redirectAttrs.addFlashAttribute("mensaje", "Registro de empresa fallido, intente nuevamente")
+                    .addFlashAttribute("clase", "success");
+            return "redirect:/crearOferta.html";
+        } else {
+            redirectAttrs.addFlashAttribute("mensaje", "Registro de empresa Exitoso").addFlashAttribute("clase",
+                    "success");
+            return "redirect:/crearOferta.html";
+        }
+    }
+
+    @PostMapping(value = "/crearOfertaCompleta")
+    public String crearOfertaCompleta(@RequestParam(name = "descripcion", required = true) String descipcion,
+            @RequestParam(name = "sueldo", required = true) String sueldo,
+            @RequestParam(name = "tiempo", required = true) String tiempo,
+            @RequestParam(name = "contrato", required = true) String tipo, RedirectAttributes redirectAttrs) {
+
+        String response = AESEncript.decrypt(controlador
+                .action(AESEncript.encrypt("5/" + key + "/" + descipcion + "/" + tipo + "/" + tiempo + "/" + sueldo)));
+        System.out.println(response);
+
+        if (response.equals("Usted no tiene acceso para realizar esta acción!!!")
+        || response.equals("La sesion no existe, ha caducado.")|| response== null) {
+            // Mala
+            redirectAttrs.addFlashAttribute("mensaje", "Registro de empresa fallido, intente nuevamente")
+                    .addFlashAttribute("clase", "success");
+            return "redirect:/crearOferta.html";
+
+        } else {
+            // && sueldo != null && tiempo != null && tipoContrato != null)
+            redirectAttrs.addFlashAttribute("mensaje", "Empresa creada correctamente").addFlashAttribute("clase", "success");
+            return "redirect:/crearOferta.html";
+        }
+    }
+
+    @PostMapping(value = "/modificarOferta")
+    public String modificarOferta() {
+
+        String response = AESEncript.decrypt(controlador.action(AESEncript.encrypt("4/" + key)));
+        System.out.println(response);
+
+        if (response.equals("Usted no tiene acceso para realizar esta acción!!!")
+                || response.equals("La sesion no existe, ha caducado.")) {
+            // Mala
+            return "";
+        } else {
+            // Buena
+            return "";
+        }
+    }
+
+    @PostMapping(value = "/buscarOfertaAspirante")
+    public String buscarOfertaAspirante() {
+
+        String response = AESEncript.decrypt(controlador.action(AESEncript.encrypt("4/" + key)));
+        System.out.println(response);
+
+        if (response.equals("Usted no tiene acceso para realizar esta acción!!!")
+                || response.equals("La sesion no existe, ha caducado.")) {
+            // Mala
+            return "";
+        } else {
+            // Buena
+            return "";
+        }
     }
 
 }
