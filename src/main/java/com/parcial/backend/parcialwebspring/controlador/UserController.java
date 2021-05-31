756b1f436a6bf429756d7e32a9fc40be36e134d5
David <davidbahe01@gmail.com>
2021-05-30 22:34:50 -0500

    aldo alli y de aca

--- src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
+++ src/main/java/com/parcial/backend/parcialwebspring/controlador/UserController.java
@@ -1,69 +1,151 @@
 package com.parcial.backend.parcialwebspring.controlador;
 
+import com.parcial.backend.parcialwebspring.entities.Usuario;
+import com.parcial.backend.parcialwebspring.services.Facade;
+import com.parcial.backend.parcialwebspring.services.proxy.AESEncript;
 import com.parcial.backend.parcialwebspring.services.proxy.Login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
-    
+    private Usuario user = null;
+    private String key = "";
+    private Facade controlador = Facade.getFacade();
+    private Login proxy = Login.getLogin();
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "Index";
     }
+
     @GetMapping("/aspirante.html")
-    public String ingresoAs(){
-        return "aspirante/aspirante";   
+    public String ingresoAs() {
+        return "aspirante/aspirante";
     }
+
     @GetMapping("/administrador.html")
-    public String ingresoAd(){
-        return "administrador/administrador";   
+    public String ingresoAd() {
+        return "administrador/administrador";
     }
-    @GetMapping("/buscarOfertaAdmin.html")
-    public String buscarOfertaAdmin(){
-        return "administrador/buscarOfertaAdmin"; 
+
+    @GetMapping("buscarOferta.html")
+    public String buscarOferta() {
+        return "buscarOferta";
     }
-    @GetMapping("/buscarOferta.html")
-    public String buscarOferta(){
-        return "buscarOferta"; 
-    }
+
     @GetMapping("crearEmpresa.html")
-    public String crearEmpresa(){
-        return "administrador/crearEmpresa";    
+    public String crearEmpresa() {
+        return "administrador/crearEmpresa";
     }
+
     @GetMapping("crearOferta.html")
-    public String crearOferta(){
-        return "administrador/crearOferta"; 
+    public String crearOferta() {
+        return "administrador/crearOferta";
     }
-    @GetMapping("modificarOferta.html")
-    public String modificarOferta(){
-        return "administrador/modificarOferta"; 
+
+    @GetMapping("modificadorOferta.html")
+    public String modificarOferta() {
+        return "administrador/modificarOferta";
     }
 
     @PostMapping(value = "/login")
-    @ResponseBody
+
     public String login(@RequestParam(name = "userLogin", required = true, defaultValue = "no envio nada") String user,
             @RequestParam(name = "passLogin", required = true, defaultValue = "no envio nada") String password,
-            @RequestParam(name = "tipologin", required = true, defaultValue = "no envio nada") String tipo) {
-        System.out.println(user + "  " + password + " " + tipo);
-        return "se logueo";
+            @RequestParam(name = "tipologin", required = true, defaultValue = "no envio nada") String tipo,
+            RedirectAttributes redirectAttrs) {
+        if (user != null) {
+            redirectAttrs.addFlashAttribute("mensaje", "Ha ingresado correctamente!!").addFlashAttribute("clase",
+                    "success");
+            return "redirect:/administrador.html";
+        } else {
+            redirectAttrs.addFlashAttribute("mensaje", "Registro fallido, intente nuevamente")
+                    .addFlashAttribute("clase", "success");
+            return "redirect:/";
+        }
     }
 
     @PostMapping(value = "/registro")
-    @ResponseBody
+
     public String asdf(
             @RequestParam(name = "userRegistro", required = false, defaultValue = "no envio nada") String user,
-            @RequestParam(name = "passRegistro", required = false, defaultValue = "no envio nada") String password, RedirectAttributes redirectAttrs ) {
-        System.out.println(user + "  " + password);
-        redirectAttrs
-            .addFlashAttribute("mensaje", "Agregado correctamente")
-            .addFlashAttribute("clase", "success");
-        return "se registro";
+            @RequestParam(name = "passRegistro", required = false, defaultValue = "no envio nada") String password,
+            RedirectAttributes redirectAttrs) {
+        if (user != null) {
+            redirectAttrs.addFlashAttribute("mensaje", "Se ha registrado correctamente!!").addFlashAttribute("clase",
+                    "success");
+            return "redirect:/";
+        } else {
+            redirectAttrs.addFlashAttribute("mensaje", "Registro fallido, intente nuevamente")
+                    .addFlashAttribute("clase", "success");
+            return "redirect:/";
+        }
     }
-    
+
+    @PostMapping(value = "/crearEmpresa")
+    public String crearEmpresa(
+            @RequestParam(name = "nameEmpresa", required = true, defaultValue = "no envio nombre") String nameEmpresa,
+            @RequestParam(name = "nit", required = true, defaultValue = "no envio nada") String nit,
+            @RequestParam(name = "direccion", required = true, defaultValue = "no envio nada") String direccion,
+            RedirectAttributes redirectAttrs) {
+        if (nameEmpresa != null) {
+            redirectAttrs.addFlashAttribute("mensaje", "Empresa creada correctamente").addFlashAttribute("clase",
+                    "success");
+            return "redirect:/crearEmpresa.html";
+        } else {
+            redirectAttrs.addFlashAttribute("mensaje", "Registro de empresa fallido, intente nuevamente")
+                    .addFlashAttribute("clase", "success");
+            return "redirect:/crearEmpresa.html";
+        }
+    }
+
+    @PostMapping(value = "/crearOfertaBase")
+    public String crearOfertaBase(RedirectAttributes redirectAttrs) {
+
+        String response = AESEncript.decrypt(controlador.action(AESEncript.encrypt("4/" + key)));
+        System.out.println(response);
+
+        if (response.equals("Usted no tiene acceso para realizar esta acción!!!")
+                || response.equals("La sesion no existe, ha caducado.")) {
+                    redirectAttrs.addFlashAttribute("mensaje", "Registro de empresa fallido, intente nuevamente")
+                    .addFlashAttribute("clase", "success");
+            return "redirect:/crearOferta.html";
+        } else {
+            redirectAttrs.addFlashAttribute("mensaje", "Registro de empresa Exitoso")
+            .addFlashAttribute("clase", "success");
+            return "redirect:/crearOferta.html";
+        }
+    }
+
+    @PostMapping(value = "/crearOfertaCompleta")
+    public String crearOfertaCompleta(
+            @RequestParam(name = "descripcion", required = true, defaultValue = "no envio nada") String descripcion,
+            @RequestParam(name = "sueldo", required = true, defaultValue = "no envio nada") String sueldo,
+            @RequestParam(name = "tiempo", required = true, defaultValue = "no envio nada") String tiempo,
+            @RequestParam(name = "tipoContrato", required = true, defaultValue = "no envio nada") String tipoContrato,
+            RedirectAttributes redirectAttrs) {
+
+        String response = AESEncript.decrypt(controlador.action(AESEncript.encrypt("5/" + key)));
+        System.out.println(response);
+
+        if (response.equals("Usted no tiene acceso para realizar esta acción!!!")
+        || response.equals("La sesion no existe, ha caducado.")|| response== null) {
+            // Mala
+            redirectAttrs.addFlashAttribute("mensaje", "Registro de empresa fallido, intente nuevamente")
+                    .addFlashAttribute("clase", "success");
+            return "redirect:/crearOferta.html";
+
+        } else {
+            // && sueldo != null && tiempo != null && tipoContrato != null)
+            redirectAttrs.addFlashAttribute("mensaje", "Empresa creada correctamente").addFlashAttribute("clase", "success");
+            return "redirect:/crearOferta.html";
+        }
+
+    }
 }
